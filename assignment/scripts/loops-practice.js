d941197cd5f9d478868f1067e1e9330e2c7dfb86
Madeline Prickette <madelineprickette@gmail.com>
2022-06-25 14:28:35 -0500

    Worked on some for loops yea yahhhh

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,18 +13,34 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+for(i=0; i<6; i++) {
+  console.log(i);
+}// I needed to change the limit of i to 6 so I could count up, including the number 5.
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for(i=3; i<6; i++) {
+  console.log(i);
+}// I changed the start number to 3 and the limit to 6. This allows 3, 4, 5 to be counted.
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for(i=2; i<11; i++) {
+  if(i%2 === 0) {
+    console.log(i);
+  }
+}// Here I changed the start number to 2, the limit to 11, and actually added an if statement.
+ // I did this because I had to console log only the even numbers, so for every i that had a 
+ // remainder of 0 when divided by 2, should be printed. 
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for(i=5; i>(-1); i--){
+  console.log(i);
+}// Here I changed the start number to 5 since we are counting down. Then chaged the limit or
+ // stop point to -1 which I placed in parenthesis to contain the -1, and I decremented i.
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
