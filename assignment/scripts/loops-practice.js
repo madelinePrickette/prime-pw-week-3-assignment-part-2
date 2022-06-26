2da1816ee5569f196fbb8aad649053f61af4bf5a
Madeline Prickette <madelineprickette@gmail.com>
2022-06-25 22:08:38 -0500

    completed while loops

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -64,6 +64,16 @@ while(i < stars.length) {
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+let count = 0;
+while(count < 6){
+  console.log(count)
+  count++;
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+count = 10;
+while(count > 4) {
+  console.log(count);
+  count--;
+}
